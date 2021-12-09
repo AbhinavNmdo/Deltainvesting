3cd19c11f0d2385afda4f971d74a53545a93fadd
Abhinav Namdeo <abhaynam22@gmail.com>
2021-12-09 21:04:25 +0530

    Added Reversal Green Calculator algorithm

--- client/src/components/Calculators/Reversal.jsx
+++ client/src/components/Calculators/Reversal.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../Style/reversal.css'
+import '../../../node_modules/animate.css/animate.css';
 
 const Reversal = () => {
+
+    const [value, setValue] = useState({first: 0, second: 0, third: 0, fourth: 0, fifth: 0})
+    const [input, setInput] = useState({gcmp: value.gcmp})
+
     const handleOnClick1 = (e) => {
         const greenc = document.querySelector('#greenc')
         const redc = document.querySelector('#redc')
@@ -28,17 +33,198 @@ const Reversal = () => {
             redb.classList.remove('redactive')
         }
     }
+
+    const handleChange = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value })
+    }
+
+    const handleOnSubmit = (e)=>{
+        e.preventDefault();
+        const gcmp = parseFloat(input.gcmp)
+        const gr = parseFloat(input.gr)
+        const gcltp = parseFloat(input.gcltp)
+        const gcd = parseFloat(input.gcd)
+        const gpltp = parseFloat(input.gpltp)
+        const gpd = parseFloat(input.gpd)
+
+        const callValue = gcltp * gcd;
+        const putValue = gpltp * gpd;
+        const netReversal = callValue - putValue;
+
+        const first = gcmp - (netReversal);
+        const second = first * 1.001
+        const third = first * 1.002
+        const fourth = first * 1.003
+        const fifth = first * 1.004
+
+        const firstr = Math.round((first + Number.EPSILON) * 100) / 100
+        const secondr = Math.round((second + Number.EPSILON) * 100) / 100
+        const thirdr = Math.round((third + Number.EPSILON) * 100) / 100
+        const fourthr = Math.round((fourth + Number.EPSILON) * 100) / 100
+        const fifthr = Math.round((fifth + Number.EPSILON) * 100) / 100
+
+        setValue(value=>({...value, first: firstr}))
+        setValue(value=>({...value, second: secondr}))
+        setValue(value=>({...value, third: thirdr}))
+        setValue(value=>({...value, fourth: fourthr}))
+        setValue(value=>({...value, fifth: fifthr}))
+    }
+
     return (
         <>
-            <div className="container-fluid" style={{ height: '75vh' }}>
+            <div className="container-fluid calc" style={{ minHeight: '70vh' }}>
                 <div className="row h-100">
-                    <div className="col-md-4 d-flex flex-column justify-content-center align-items-center">
-                        <div id="greenb" className="greenbutton" onClick={handleOnClick2}>Green Calc</div>
-                        <div id="redb" className="redbutton" onClick={handleOnClick1}>Red Calc</div>
+                    <div className="col-md-3 d-flex flex-column justify-content-center align-items-center">
+                            {/* <h3 >Reversal Calculator</h3> */}
+                        <div id="buttons">
+                            <div id="greenb" className="greenbutton greenactive" onClick={handleOnClick2}>Green Calc</div>
+                            <div id="redb" className="redbutton" onClick={handleOnClick1}>Red Calc</div>
+                        </div>
                     </div>
-                    <div className="col-md-8 d-flex justify-content-center align-items-center" style={{ transition: 'all' }}>
-                        <div id="redc" className="card d-none" style={{ height: '400px', width: '400px', background: 'red' }}></div>
-                        <div id="greenc" className="card d-none" style={{ height: '400px', width: '400px', background: 'rgb(0, 255, 0)' }}></div>
+                    <div className="col-md-9 d-flex justify-content-center align-items-center">
+                        <div id="greenc" className="card greencard p-3">
+                            <div className="row">
+                                <div className="col-md-7 pe-5">
+                                    <form onSubmit={handleOnSubmit}>
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="gcmp" className="col-form-label mt-4">Current Market Price: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="text" id="gcmp" name="gcmp" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="gr" className="col-form-label mt-4">Support: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="text" id="gr" name="gr" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="gcltp" className="col-form-label mt-4">Call LTP: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="text" id="gcltp" name="gcltp" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="gcd" className="col-form-label mt-4">Call Delta: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="text" id="gcd" name="gcd" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="gpltp" className="col-form-label mt-4">Put LTP: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="text" id="gpltp" name="gpltp" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="gpd" className="col-form-label mt-4">Put Delta: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="text" id="gpd" name="gpd" onChange={handleChange} />
+                                            </div>
+                                        </div>
+                                        <button className="btn btn-primary" type="submit">Submit</button>
+                                    </form>
+                                </div>
+                                <div className="col-md-5 d-flex justify-content-center align-items-center ranges">
+                                    <div className="d-flex flex-column justify-content-center align-items-center">
+                                        <h4 className="text-center mb-5">Reversal Price Range</h4>
+                                        <h6>{value.first}</h6>
+                                        <h6>{value.second}</h6>
+                                        <h6>{value.third}</h6>
+                                        <h6>{value.fourth}</h6>
+                                        <h6>{value.fifth}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="redc" className="card d-none redcard p-4">
+                            <div className="row">
+                                <div className="col-md-7 pe-5">
+                                    <form>
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="rcmp" className="col-form-label mt-4">Current Market Price: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="number" id="rcmp" name="rcmp" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="rr" className="col-form-label mt-4">Resistance: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="number" id="rr" name="rr" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="rcltp" className="col-form-label mt-4">Call LTP: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="number" id="rcltp" name="rcltp" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="rcd" className="col-form-label mt-4">Call Delta: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="number" id="rcd" name="rcd" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="rpltp" className="col-form-label mt-4">Put LTP: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="number" id="rpltp" name="rpltp" onChange={handleChange} />
+                                            </div>
+                                        </div>
+
+                                        <div className="row g-3 align-items-center">
+                                            <div className="col">
+                                                <label htmlFor="rpd" className="col-form-label mt-4">Put Delta: </label>
+                                            </div>
+                                            <div className="col">
+                                                <input className="form-control mt-4" type="number" id="rpd" name="rpd" onChange={handleChange} />
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="col-md-5 d-flex justify-content-center align-items-center ranges">
+                                    <div className="d-flex flex-column justify-content-center align-items-center">
+                                        <h4 className="text-center mb-5">Reversal Price Range</h4>
+                                        <h6>345345</h6>
+                                        <h6>345345</h6>
+                                        <h6>345345</h6>
+                                        <h6>345345</h6>
+                                        <h6>345345</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
