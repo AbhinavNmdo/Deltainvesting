e8c7eda5f3203bfcd760d50f906c8f0b95afc433
Abhinav Namdeo <abhaynam22@gmail.com>
2021-12-11 14:05:26 +0530

    Added support for images upload in classes with express-fileupload

--- routes/class.js
+++ routes/class.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const Class = require("../models/Class");
 const fetchuser = require("../middleware/fetchuser");
+const fileupload = require("express-fileupload");
 const router = express.Router();
+router.use(fileupload());
 
 router.get("/", async (req, res) => {
   try {
@@ -21,26 +23,50 @@ router.get("/class/:id", async (req, res) => {
 
 router.post("/", (req, res) => {
   try {
-    const { name, description, classLink, thumbnail } = req.body;
-    let classes = Class.create({
-      name,
-      description,
-      classLink,
-      thumbnail,
-    });
-    res.status(201).json({success: true, classes});
+    if (req.files) {
+      const { name, description, classLink } = req.body;
+      let file = req.files.file;
+      let classes = Class.create({
+        name,
+        description,
+        classLink,
+        thumbnail: file.name,
+      });
+      file.mv(
+        "../deltainvesting/client/src/Images/upload/" + file.name,
+        function (err, result) {
+          if (err && classes) {
+            res
+              .status(500)
+              .json({ success: false, error: "Failed to upload image" });
+          } else {
+            res.status(201).json({ success: true, classes });
+          }
+        }
+      );
+    }
+    else{
+      const { name, description, classLink } = req.body;
+      let classes = Class.create({
+        name,
+        description,
+        classLink,
+        thumbnail: null,
+      });
+    }
   } catch (error) {
-    res.status(500).json({success: false, error: "internal error" });
+    console.log(error);
+    res.status(500).json({ success: false, error: "internal error" });
   }
 });
 
-router.delete('/class/:id', async (req, res)=>{
-    try {
-        let classes = await Class.findByIdAndDelete(req.params.id);
-        res.status(200).json({success: true, classes});
-    } catch (error) {
-        res.status(500).json({success: false, error: "internal error"})
-    }
-})
+router.delete("/class/:id", async (req, res) => {
+  try {
+    let classes = await Class.findByIdAndDelete(req.params.id);
+    res.status(200).json({ success: true, classes });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "internal error" });
+  }
+});
 
 module.exports = router;
