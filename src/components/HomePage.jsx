86f9d22a43d30a8932d05ed31defa71839251b79
AbhinavNmdo <abhaynam22@gmail.com>
2022-01-20 10:31:28 +0530

    Added custom scrollbar using css only

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -22,42 +22,42 @@ const HomePage = (props) => {
   const [classes, setClasses] = useState([]);
   const [review, setReview] = useState([]);
 
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    navigator: true,
-    slidesToShow: 3,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1,
-        }
-      }
-    ]
-  };
+  // const settings = {
+  //   dots: true,
+  //   infinite: false,
+  //   speed: 500,
+  //   navigator: true,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 4,
+  //   initialSlide: 0,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         initialSlide: 2
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         initialSlide: 1,
+  //       }
+  //     }
+  //   ]
+  // };
 
   const getClass = async () => {
     props.setProgress(50)
